adde5fd494b2339c33841eaeb25337cfd67efe67
ed ben <edwardbenedict92@gmail.com>
2021-05-29 17:54:51 +0300

    feat: finish first class

---
+++ src/components/header/Form.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button, FoodInput, FormContainer, Select } from "./headerStyle";
+
+const Form = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("handleSubmit");
+    // getData()
+  };
+  return (
+    <FormContainer onSubmit={handleSubmit}>
+      <FoodInput type="text" placeholder="Search" />
+      <Button>Search</Button>
+      <Select name="mealTypes" id="mealTypes"></Select>
+    </FormContainer>
+  );
+};
+
+export default Form;
